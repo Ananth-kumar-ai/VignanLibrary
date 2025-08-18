5a9899ea166528ac744431c56e0d9ee691a7823c
Ananth-kumar-ai <ananthkumarnalluri456@gmail.com>
2025-08-18 09:14:21 +0530

    Added dashboard

---
+++ app/src/main/java/org/vignanuniversity/vignanlibrary/Fragments/HomeFragment.java
@@ -0,0 +1,20 @@
+package org.vignanuniversity.vignanlibrary.Fragments;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.vignanuniversity.vignanlibrary.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
--- app/src/main/java/org/vignanuniversity/vignanlibrary/MainActivity.java
+++ app/src/main/java/org/vignanuniversity/vignanlibrary/MainActivity.java
@@ -1,24 +1,53 @@
 package org.vignanuniversity.vignanlibrary;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import org.vignanuniversity.vignanlibrary.R;
+
+import org.vignanuniversity.vignanlibrary.Fragments.BorrowFragment;
+import org.vignanuniversity.vignanlibrary.Fragments.CatalogFragment;
+import org.vignanuniversity.vignanlibrary.Fragments.HomeFragment;
+import org.vignanuniversity.vignanlibrary.Fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (id == R.id.nav_catalog) {
+                selectedFragment = new CatalogFragment();
+            } else if (id == R.id.nav_borrow) {
+                selectedFragment = new BorrowFragment();
+            } else if (id == R.id.nav_profile) {
+                selectedFragment = new ProfileFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
         });
     }
 }
