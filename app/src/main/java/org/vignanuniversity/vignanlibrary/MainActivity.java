acf49780e018502d146e2399ec84b679852fa01a
Ananth-kumar-ai <ananthkumarnalluri456@gmail.com>
2025-08-20 14:24:40 +0530

    Added nav

--- app/src/main/java/org/vignanuniversity/vignanlibrary/MainActivity.java
+++ app/src/main/java/org/vignanuniversity/vignanlibrary/MainActivity.java
@@ -11,6 +11,7 @@ import org.vignanuniversity.vignanlibrary.Fragments.BorrowFragment;
 import org.vignanuniversity.vignanlibrary.Fragments.CatalogFragment;
 import org.vignanuniversity.vignanlibrary.Fragments.HomeFragment;
 import org.vignanuniversity.vignanlibrary.Fragments.ProfileFragment;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         // Set default fragment
@@ -26,26 +31,38 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, new HomeFragment())
                     .commit();
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().setTitle("Vignan Library");
+            }
         }
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             Fragment selectedFragment = null;
+            String title = "";
             int id = item.getItemId();
 
             if (id == R.id.nav_home) {
                 selectedFragment = new HomeFragment();
+                title = "Home";
             } else if (id == R.id.nav_catalog) {
                 selectedFragment = new CatalogFragment();
+                title = "Catalog";
             } else if (id == R.id.nav_borrow) {
                 selectedFragment = new BorrowFragment();
+                title = "Borrow";
             } else if (id == R.id.nav_profile) {
                 selectedFragment = new ProfileFragment();
+                title = "Profile";
             }
 
             if (selectedFragment != null) {
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, selectedFragment)
                         .commit();
+
+                if (getSupportActionBar() != null) {
+                    getSupportActionBar().setTitle(title);
+                }
             }
             return true;
         });
